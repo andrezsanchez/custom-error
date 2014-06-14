e2f4dbdda1d0633dfe607a25849dc954dd9561e4
Andre Z. Sanchez <andrezacsanchez@gmail.com>
2014-06-14 00:25:12 -0400

    Add test for stack message

--- test/message.js
+++ test/message.js
@@ -2,8 +2,13 @@ var test = require('tape')
 var error = require('../')
 
 test('message', function(t) {
-  t.plan(2)
+  t.plan(3)
   var MyError = error('MyError')
   t.equals(MyError().name, 'MyError', 'name should be correct')
   t.equals(MyError('rawr').message, 'rawr', 'message should be correct')
+
+  t.ok(
+    /^MyError: meh/.test(MyError('meh').stack),
+    'stack should contain name and message'
+  )
 })
