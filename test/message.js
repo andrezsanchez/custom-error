a6161129aef008c418b6358d5fed2bfb74e4760d
Andre Z. Sanchez <andrezacsanchez@gmail.com>
2014-06-14 12:18:29 -0400

    Change message test to be more readable on errors

--- test/message.js
+++ test/message.js
@@ -7,8 +7,5 @@ test('message', function(t) {
   t.equals(MyError().name, 'MyError', 'name should be correct')
   t.equals(MyError('rawr').message, 'rawr', 'message should be correct')
 
-  t.ok(
-    /^MyError: meh/.test(MyError('meh').stack),
-    'stack should contain name and message'
-  )
+  t.equals(MyError('meh').stack.split('\n')[0], 'MyError: meh', 'stack should contain name and message')
 })
