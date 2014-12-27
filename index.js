e81e900c711342a97324a95f92b9df6fc0f9430d
Andre Z. Sanchez <andrezacsanchez@gmail.com>
2014-12-27 12:33:42 -0500

    Explain try/catch block

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ function ErrorMaker(name, ParentError) {
     if (!(this instanceof NewError))
       return new NewError(message)
 
-    // get stack
+    // Use a try/catch block to capture the stack trace. Capturing the stack trace here is
+    // necessary, otherwise we will get the stack trace at the time the new error class was created,
+    // rather than when it is instantiated.  We add `message` and `name` so that the stack trace
+    // string will match our current error class.
     try {
       throw new Error(message)
     }
